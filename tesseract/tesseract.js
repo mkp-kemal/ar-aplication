491893a2260abf5efbcc0626d48a9275d48793f2
mkp-kemal <kemal.garut7@gmail.com>
2024-06-27 10:00:58 +0700

    fix upload image

--- tesseract/tesseract.js
+++ tesseract/tesseract.js
@@ -1,12 +1,30 @@
 import T from 'tesseract.js'
+import axios from 'axios'
 
 export const predictText = async (image, res) => {
     try {
         const result = await T.recognize(image, 'ind', {});
-        const resultText = result.data.text.split('\n');
-        res.json({ result: resultText });
+        let resultText = result.data.text;
+        resultText = resultText.replace(/\n/g, ' ');
+
+        const response = await axios.get(`http://localhost:5000/v1/api/hadits/${resultText}`)
+            .catch((error) => {
+                if (error.response && error.response.status === 404) {
+                    return {
+                        data: []
+                    };
+                }
+                throw error;
+            });
+        if (response.data.length === 0) {
+            return res.status(404).json({ message: 'Hadits tidak ditemukan, coba gambar lain' });
+        }
+        res.json(response.data);
     } catch (error) {
+        if (error.response && error.response.status === 404) {
+            return res.status(404).json({ message: 'Hadits tidak ditemukan, coba gambar lain' });
+        }
         console.error(error);
-        return null;
+        return res.status(500).json({ message: error.message });
     }
 }
